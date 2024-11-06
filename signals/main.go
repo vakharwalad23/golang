17c1ff2f3b6cb729f874c252d08f616c24ebe40a
vakharwalad23 <vakharwala.d@gmail.com>
2024-11-07 02:13:50 +0530

    feat: Add signal handling to gracefully exit on SIGINT in Go

---
+++ signals/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT)
+
+	done := make(chan bool, 1)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	fmt.Println("waiting for signal")
+	<-done
+	fmt.Println("exit")
+}
